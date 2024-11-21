20392af3ca043d8757c91da63cdcfe65918f728a
Daniil Pogosyan <daniilpogosyan@gmail.com>
2024-11-21 18:28:05 +0000

    Feat: Implement get items route

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import Fastify from "fastify";
+import { CacheManagerPlugin } from "./infrastructure/cache-manager/cache-manager.plugin";
+import { SkinportRequesterPlugin } from "./infrastructure/skinport-requester/skinport-requester.plugin";
+import { ItemModule } from "./modules/item/item.module";
+import { UserModule } from "./modules/user";
 
 const fastify = Fastify();
 
-fastify.post("/buy", function (request, reply) {
-  reply.send({ hello: "world" });
-});
+fastify.register(CacheManagerPlugin);
+fastify.register(SkinportRequesterPlugin);
+
+fastify.register(ItemModule);
+fastify.register(UserModule);
 
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
--- src/modules/item/item.service.ts
+++ src/modules/item/item.service.ts
@@ -1,10 +1,30 @@
 import { FastifyPluginCallback } from "fastify";
 import fp from "fastify-plugin";
+import { ICacheManager } from "../../infrastructure/cache-manager/cache-manager.interface";
+import { GetItemsOutput } from "../../infrastructure/skinport-requester/skinport-requester.dto";
+import { ISkinportRequester } from "../../infrastructure/skinport-requester/skinport-requester.interface";
 import { ITEM_SERVICE } from "./item.const";
 
 export class ItemService {
-  getItems() {
-    return [1, 2, 3];
+  constructor(
+    private skinportRequester: ISkinportRequester,
+    private cacheManager: ICacheManager
+  ) {}
+
+  async getItems() {
+    const ITEMS_CACHE_KEY = "SKINPORT_ITEMS";
+    const ITEMS_CACHE_TTL = 300_000;
+
+    let items = await this.cacheManager.get<GetItemsOutput[]>(ITEMS_CACHE_KEY);
+
+    if (items) {
+      return items;
+    }
+
+    items = await this.skinportRequester.getItems();
+    await this.cacheManager.set(ITEMS_CACHE_KEY, items, ITEMS_CACHE_TTL);
+
+    return items;
   }
 
   buyItem() {
@@ -19,5 +39,8 @@ declare module "fastify" {
 }
 
 export const ItemServicePlugin: FastifyPluginCallback = fp(function (fastify) {
-  fastify.decorate(ITEM_SERVICE, new ItemService());
+  fastify.decorate(
+    ITEM_SERVICE,
+    new ItemService(fastify.skinportRequester, fastify.cacheManager)
+  );
 });
