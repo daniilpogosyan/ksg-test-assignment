fe8531805d2caf1102e04ce120cb801446d63412
Daniil Pogosyan <daniilpogosyan@gmail.com>
2024-11-22 06:00:16 +0000

    Feat: Parse request body in buyItem

--- src/modules/item/routes/buy-item.route.ts
+++ src/modules/item/routes/buy-item.route.ts
@@ -1,7 +1,25 @@
 import { FastifyPluginCallback } from "fastify";
 
 export const BuyItemRoute: FastifyPluginCallback = function (fastify) {
-  fastify.post("/buy-item", (request, reply) => {
-    return fastify.itemService.buyItem(1, 100);
-  });
+  fastify.post<{ Body: { price: number; userId: number } }>(
+    "/buy-item",
+    {
+      schema: {
+        body: {
+          type: "object",
+          required: ["userId", "price"],
+          properties: {
+            userId: { type: "integer", minimum: 0 },
+            price: { type: "number", minimum: 0 },
+          },
+        },
+      },
+    },
+    (request, reply) => {
+      return fastify.itemService.buyItem(
+        request.body.userId,
+        request.body.price
+      );
+    }
+  );
 };
